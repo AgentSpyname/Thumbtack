659f5290d11b005c06348ebb7bae57b18a43e755
Alexander Parsan <alexanderparsan28@gmail.com>
2016-05-13 21:46:23 +0000

    remove page caching

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public/deploy"
     config.assets.initialize_on_precompile = false
 
 
