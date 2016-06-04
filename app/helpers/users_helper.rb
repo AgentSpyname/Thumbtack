4a08d5c795bd68384589eb58fcc0e3d19322c58a
Alexander Parsan <alexanderparsan28@gmail.com>
2016-06-04 01:53:58 +0000

    Added users posts to users show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,9 @@
 module UsersHelper
     def user_list()
-        {"user_name" => @user.name, "user_bio" => @user.bio, "user_email" => @user.contact_email, "user_title" => @user.title}
+        user_post_list = Array.new
+        @user.posts.each do |post|
+        user_post_list.push({"title" => post.title, "url" => post.full_url, "content" => post.content})
+        end
+        {"user_name" => @user.name, "user_bio" => @user.bio, "user_email" => @user.contact_email, "user_title" => @user.title, "posts" => user_post_list}
     end
 end
