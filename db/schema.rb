2c9dc4d1c27314e43f2c8c1fe4539002720664ec
Alexander Parsan <alexanderparsan28@gmail.com>
2016-06-13 18:50:47 +0000

    New gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160605020412) do
+ActiveRecord::Schema.define(version: 20160604124327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
